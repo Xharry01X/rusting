e6aaa105ff6a80cb7bde14de2aa1af4e14939b5f
Xharry01X <harshitsingh50621@gmail.com>
2025-01-11 21:55:50 +0530

    self made functions .

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,11 +1,18 @@
-use std::str;
 
-fn main(){
-   let message = "Hey there what are you doing";
-  print_message(message);
 
+fn main() {
+    let age = 12;
+    finding_age(age);
 }
 
-fn print_message(text: &str){
-    println!("{}",text)
+fn finding_age(age: i32) {
+    for n in 1..=15 {
+        if n > age {
+            println!("you are senior to me: {}", n);
+        }else if n == age {
+            println!("your age is similar to me: {}", n);
+        }else {
+            println!("You are junior to  me: {}", n);
+        }
+    }
 }
