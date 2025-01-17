1cc3b9095e5bc43ded7874b0cc5737727efbb1b1
Xharry01X <harshitsingh50621@gmail.com>
2025-01-17 22:55:51 +0530

    simple binary calculation by rust

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,14 +1,17 @@
 
 
 fn main() {
-    let message = "Hello world";
-        let message2 = print_message(message);
-        println!("{}", message2);
+
+    let custom_num = 98_000; // it is just a separator so that it look big number, other you can write without _
+    let hex_num = 0xfa;  // 0x is says hexdecimal fomating, check value of f and a and solve
+    let bin_num = 0b0010_1011; // 0b says it is a binary number try to solve it 
+    let byte_num = b'A'; // 'A' is UTF encoded try check table value 41, 0x41 solve it get 65
+
+    println!("{}",custom_num);
+    println!("{}",hex_num);
+    println!("{}",bin_num);
+    println!("{}",byte_num);
+    
 }
 
-fn print_message(text: &str) -> &str {
-    println!("{}", text);
 
-    let new_message = "heyy please";
-     new_message
-}
