01378ccd17b8351b567ee91f7b54bd155fac125f
Xharry01X <harshitsingh50621@gmail.com>
2025-01-17 23:02:24 +0530

    tuple example in rust

--- snakii/main.rs
+++ snakii/main.rs
@@ -2,15 +2,13 @@
 
 fn main() {
 
-    let custom_num = 98_000; // it is just a separator so that it look big number, other you can write without _
-    let hex_num = 0xfa;  // 0x is says hexdecimal fomating, check value of f and a and solve
-    let bin_num = 0b0010_1011; // 0b says it is a binary number try to solve it 
-    let byte_num = b'A'; // 'A' is UTF encoded try check table value 41, 0x41 solve it get 65
+    let tup = (20,"harry",4.343);
 
-    println!("{}",custom_num);
-    println!("{}",hex_num);
-    println!("{}",bin_num);
-    println!("{}",byte_num);
+    let (a,b,c) = tup;
+
+    println!("{}",a);
+    println!("{}",b);
+    println!("{}",c);
     
 }
 
