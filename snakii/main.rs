1991c30e0dc21bdf6cf4b4ad1967b504c5e0f75f
Xharry01X <harshitsingh50621@gmail.com>
2025-01-24 21:04:33 +0530

    learning moving and borrowing

--- snakii/main.rs
+++ snakii/main.rs
@@ -2,19 +2,11 @@
 
 fn main() {
 
-   let a = 5;
-   let b = 6;
-
-   let sum = add(a,b);
-
-   println!("Sum of two number is {}", sum);
+     let a =String::from("Hello world");
+     let b = a;
+     println!("{}",a);
 }
 
-fn add(x:i32,y:i32) -> i32 {
-  
-   let result = x + y;
 
-  return result;
-}
 
 
