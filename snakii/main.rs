706dbcef1c7b2a921b92af6811afc81b7487ec5b
Xharry01X <harshitsingh50621@gmail.com>
2025-01-24 23:01:51 +0530

    memory allocation and deallocation

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,12 +1,23 @@
 
 
-fn main() {
+fn main(){
 
-     let a =String::from("Hello world");
-     let b = a;
-     // cannot print a because value moved from a to b
-     println!("{}",a);
+   let message = String::from("Hello"); // message coming to the scope (into heap).
+
+   print_message(message); // message is moved into print_message function.
+   //message is no longer valid
 }
+
+// message is going out of scope
+// but nothing more will happen because it was moved into print_message
+
+fn print_message(a: String){ // a comes into the scope (heap)
+   println!("{}",a);  
+   let c = a; // a is moved into the c and c is coming into the scope(heap)
+
+   // a is no longer valid
+} // a is going out of the scope, nothing more will happen because it was moved 
+// c is going out of the scope and 'drop' called which clears the memory from the heap.
 
 
 
