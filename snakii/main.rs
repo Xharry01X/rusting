9dc7ac5a9cbde941d3994a3fc07c6ce08af7d56c
Xharry01X <harshitsingh50621@gmail.com>
2025-01-29 21:00:40 +0530

    return moved string

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,23 +1,26 @@
 
 
 fn main(){
+   // now I'm able to get back my message it removed previusly
 
    let message = String::from("Hello"); // message coming to the scope (into heap).
 
-   print_message(message); // message is moved into print_message function.
-   //message is no longer valid
+  let message_2 = extend_message(message);
+
+  println!("{}",message_2);
+
+  
 }
 
-// message is going out of scope
-// but nothing more will happen because it was moved into print_message
+fn extend_message(mut a: String) -> String {
+   a.push_str(" World");
+   a
+}
 
-fn print_message(a: String){ // a comes into the scope (heap)
-   println!("{}",a);  
-   let c = a; // a is moved into the c and c is coming into the scope(heap)
 
-   // a is no longer valid
-} // a is going out of the scope, nothing more will happen because it was moved 
-// c is going out of the scope and 'drop' called which clears the memory from the heap.
+
+
+
 
 
 
