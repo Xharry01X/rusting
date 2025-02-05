8d3357a4c87595cc9fdf14129471e538b19a758b
Xharry01X <harshitsingh50621@gmail.com>
2025-02-05 23:01:45 +0530

    deferencing..

--- snakii/main.rs
+++ snakii/main.rs
@@ -3,13 +3,14 @@
 fn main(){
    // now I'm able to get back my message it removed previusly
 
-  let message = String::from("Hello_world");
-  let message_2 = &message;
+  let mut message = String::from("Hello_world");
+  let message_2: &mut String = &mut message;
   // message_2 is not a owner of data
   // &message_2 is "borrowing" a reference to message
+  message_2.push_str("hello2"); // refernce is a pointer that doesnt own any data, it is just pointing to the stack.
 
   println!("{}",message);
-  println!("{}",message_2);
+//   println!("{}",message_2);
  
 // message and message_2 going out of the scope
 // message_2 is not dropped because it doesn't have ownership of actual message
