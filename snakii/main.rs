f6bca5d91322107ac1b5f1d1ccc2868e137ec1bb
Xharry01X <harshitsingh50621@gmail.com>
2025-02-05 23:19:26 +0530

    small example of borrow

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,7 +1,18 @@
+// fn main(){
+
+//    let book = String::from("Rust book");
+
+//    let borrowed_book = &book; // read only
+//    println!("Broorowd book: {}", borrowed_book);
+// }
+
+
 fn main(){
+   let mut book = String::from("Rust book");
 
-   let book = String::from("Rust book");
+   let borrowed_book = &mut book;
 
-   let borrowed_book = &book;
-   println!("Broorowd book: {}", borrowed_book);
+   borrowed_book.push_str("is alowed");
+
+   println!("{}",borrowed_book);
 }
