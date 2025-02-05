e8c3da6d6a6ac50fb76548fbbf2684376029c073
Xharry01X <harshitsingh50621@gmail.com>
2025-02-05 22:51:51 +0530

    reference and ownership-1

--- snakii/main.rs
+++ snakii/main.rs
@@ -3,19 +3,21 @@
 fn main(){
    // now I'm able to get back my message it removed previusly
 
-   let message = String::from("Hello"); // message coming to the scope (into heap).
+  let message = String::from("Hello_world");
+  let message_2 = &message;
+  // message_2 is not a owner of data
+  // &message_2 is "borrowing" a reference to message
 
-  let message_2 = extend_message(message);
-
+  println!("{}",message);
   println!("{}",message_2);
-
+ 
+// message and message_2 going out of the scope
+// message_2 is not dropped because it doesn't have ownership of actual message
+//message is dropped
   
 }
 
-fn extend_message(mut a: String) -> String {
-   a.push_str(" World");
-   a
-}
+
 
 
 
