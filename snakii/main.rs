c713e28aff12f17997c54429122641b61b3dd31a
Xharry01X <harshitsingh50621@gmail.com>
2025-02-05 23:13:42 +0530

    borrowing simple example

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,30 +1,7 @@
-
-
 fn main(){
-   // now I'm able to get back my message it removed previusly
 
-  let mut message = String::from("Hello_world");
-  let message_2: &mut String = &mut message;
-  // message_2 is not a owner of data
-  // &message_2 is "borrowing" a reference to message
-  message_2.push_str("hello2"); // refernce is a pointer that doesnt own any data, it is just pointing to the stack.
+   let book = String::from("Rust book");
 
-  println!("{}",message);
-//   println!("{}",message_2);
- 
-// message and message_2 going out of the scope
-// message_2 is not dropped because it doesn't have ownership of actual message
-//message is dropped
-  
+   let borrowed_book = &book;
+   println!("Broorowd book: {}", borrowed_book);
 }
-
-
-
-
-
-
-
-
-
-
-
