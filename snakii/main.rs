c6d63d428d73ff65d2106e95125b30107d88c3ae
Xharry01X <harshitsingh50621@gmail.com>
2025-02-11 14:00:54 +0530

    simple example to learn struct..

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,28 +1,34 @@
-// fn main(){
-
-//    let book = String::from("Rust book");
-
-//    let borrowed_book = &book; // read only
-//    println!("Broorowd book: {}", borrowed_book);
-// }
-
 struct Person {
     name: String,
     last_name: String,
     age: u32,
 }
 
+impl Person {
+    // Associated function (called on the type)
+    fn something() {
+        println!("This is an associated function.");
+    }
 
-fn main(){
- let person = Person {
-    name: "Harry".to_string(),
-    last_name: "Singh".to_string(),
-    age: 23
- };
-
- println!(" {} {} {}", person.name, person.last_name, person.age);
-
-
-   
+    // Method (called on an instance)
+    fn display_age(&self) {
+        println!("Current age is {}", self.age);
+    }
 }
 
+fn main() {
+    let person = Person {
+        name: "Harry".to_string(),
+        last_name: "Singh".to_string(),
+        age: 23,
+    };
+
+    // Call method on the instance
+    person.display_age();
+
+    // Call associated function on the type
+    Person::something();
+
+    // Print person details
+    println!("{} {} is {} years old.", person.name, person.last_name, person.age);
+}
