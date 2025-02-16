b1a62704309cf28bf102ac343a20bc5f978a234b
Xharry01X <harshitsingh50621@gmail.com>
2025-02-16 21:33:39 +0530

    revison done

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,34 +1,18 @@
+// revision of struct
+
 struct Person {
     name: String,
-    last_name: String,
-    age: u32,
+    game: String,
+    age: u32
 }
 
-impl Person {
-    // Associated function (called on the type)
-    fn something() {
-        println!("This is an associated function.");
-    }
+fn main(){
 
-    // Method (called on an instance)
-    fn display_age(&self) {
-        println!("Current age is {}", self.age);
-    }
-}
-
-fn main() {
-    let person = Person {
+     let person = Person {
         name: "Harry".to_string(),
-        last_name: "Singh".to_string(),
-        age: 23,
-    };
+        game:"football".to_string(),
+        age: 19,
+     };
 
-    // Call method on the instance
-    person.display_age();
-
-    // Call associated function on the type
-    Person::something();
-
-    // Print person details
-    println!("{} {} is {} years old.", person.name, person.last_name, person.age);
+     println!("{} {} {}",person.name, person.game, person.age);
 }
