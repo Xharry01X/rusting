0d33bc499d3cc9a63f43c4d90e1db20a5195984c
Xharry01X <harshitsingh50621@gmail.com>
2025-02-16 21:53:33 +0530

    basic concept of struct

--- snakii/main.rs
+++ snakii/main.rs
@@ -6,13 +6,34 @@ struct Person {
     age: u32
 }
 
-fn main(){
+impl Person {
+    fn some_function(){
+        println!("some_function");
+    }
 
+      // this is the instance of person
+    fn display_age(self){
+        println!("Current age is {}",self.age)
+    }
+}
+
+fn main(){
+   Person::some_function();
      let person = Person {
         name: "Harry".to_string(),
         game:"football".to_string(),
         age: 19,
      };
 
-     println!("{} {} {}",person.name, person.game, person.age);
+     let person_2 = Person {
+        name: "Carry".to_string(),
+        game: "soccer".to_string(),
+        age: 20
+     };
+
+
+     person.display_age();
+     person_2.display_age();
+
+    //  println!("{} {} {}",person.name, person.game, person.age);
 }
