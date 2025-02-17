67a6057e0bf4a540f67dfbdef00da6254c600aa6
Xharry01X <harshitsingh50621@gmail.com>
2025-02-17 21:15:11 +0530

    example - 2

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,21 +1,20 @@
-struct Temperature {
-    degrees_celsius: f64,
+struct Rectangle {
+    width: f64,
+    height: f64,
 }
 
-impl Temperature {
-    // Constructor that ensures the temperature is within a valid range
-    fn new(degrees_celsius: f64) -> Result<Self, String> {
-        if (-273.15..=1_000.0).contains(&degrees_celsius) {
-            Ok(Self { degrees_celsius })
-        } else {
-            Err(String::from("Temperature must be between -273.15 and 1000.0 degrees Celsius."))
+impl Rectangle {
+    fn new() -> Self {
+        Self {
+            width: 1.0,
+            height: 5.0,
         }
     }
 }
+fn main () {
 
-fn main() {
-    match Temperature::new(-300.0) {
-        Ok(temp) => println!("Temperature set to {}°C", temp.degrees_celsius),
-        Err(e) => println!("Error: {}", e),
-    }
+let rect = Rectangle::new();
+println!("{} {}", rect.width, rect.height);
+
 }
+
