d2dede462158a33b37c67b1e8f4d49cfbd269ffc
Xharry01X <harshitsingh50621@gmail.com>
2025-02-22 20:56:24 +0530

    basic-1

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,23 +1,11 @@
-enum Person {
-    Student(String,i32),
-    Teacher(String),
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left
 }
 
-impl Person {
-    fn describe(&self) -> String {
-        match self {
-            Person::Student(name,grade) => format!("{} is a student in grade {}",name,grade),
-            Person::Teacher(name) => format!("{} is a teacher",name),
-        }
-    }
-}
-
-fn main(){
-
-    let student = Person::Student(String::from("Harry"), 10);
-    let teacher = Person::Teacher(String::from("Shukla"));
-
-    println!("{}",student.describe());
-    println!("{}",teacher.describe());
-
+fn main (){
+    let myDirection = Direction::Right;
+    println!("We're always Right");
 }
