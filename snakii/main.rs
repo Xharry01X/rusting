c258c5801e21815f9f13b6155963cdb82a2fd66c
Xharry01X <harshitsingh50621@gmail.com>
2025-02-22 22:57:17 +0530

    basic trait implement...

--- snakii/main.rs
+++ snakii/main.rs
@@ -6,13 +6,26 @@ struct Person {
     name: String,
 }
 
+struct Robot {
+    id: i32
+}
+
 impl Speak for Person {
     fn say_hii(&self) {
         println!("Hello I'm {}",self.name);
     }
 }
 
+impl Speak for Robot {
+    fn say_hii(&self) {
+        println!("Beep boop, I'm Robot #{}",self.id);
+    }
+}
+
 fn main(){
-    let person = Person { name: String::from("Harry") };
+    let person = Person { name: String::from("Harry")};
+    let robot = Robot { id: 12 };
+
     person.say_hii();
+    robot.say_hii();
 }
