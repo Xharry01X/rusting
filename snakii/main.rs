59b458025ca1f94cc2401cca5350d8b570eb4233
Xharry01X <harshitsingh50621@gmail.com>
2025-02-22 21:03:37 +0530

    basic 1.2

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,11 +1,13 @@
-enum Direction {
-    Up,
-    Down,
-    Right,
-    Left
+enum Message {
+    Quit,
+    Move(i32,i32),
+    Say(String)
 }
 
-fn main (){
-    let myDirection = Direction::Right;
-    println!("We're always Right");
+fn main(){
+    let message1 = Message::Quit;
+    let msg2 = Message::Move(10, 20);
+    let msg3 = Message::Say(String::from("Hello!"));
+
+    println!("Message created");
 }
