a4a7136380d3a64e81f5b8b10c7da5a5b8b38aec
Xharry01X <harshitsingh50621@gmail.com>
2025-03-03 20:32:54 +0530

    basic modules..

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,18 +1,9 @@
-#[derive(PartialEq, Debug)]
-struct Student {
-    id: i32,
-    grade: char,
+mod greeting {
+    pub fn say_hello() {
+        println!("Hello world");
+    }
 }
 
-fn find_match<T: PartialEq>(a: T, b: T) -> bool {
-    a == b 
-}
-
-fn main() {
-    let student1 = Student { id: 101, grade: 'A' };
-    let student2 = Student { id: 101, grade: 'A' };
-
-    let are_same = find_match(student1, student2);
-
-    println!("Are they the same? {}", are_same); 
+fn main(){
+    greeting::say_hello();
 }
