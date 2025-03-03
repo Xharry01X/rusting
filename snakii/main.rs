b908181c43dc1479cf13763d57364e05bbbfd933
Xharry01X <harshitsingh50621@gmail.com>
2025-03-03 14:22:47 +0530

    small info..

--- snakii/main.rs
+++ snakii/main.rs
@@ -14,3 +14,11 @@ fn main(){
     let (pos1,pos2) = move_position(start);
     println!("Pos1: ({}, {}), Pos2: ({}, {})", pos1.x, pos1.y, pos2.x, pos2.y);
 }
+
+
+// #[derive(Copy, Clone)]: Copy says this struct is simple (no strings or vectors), so it’s copied instantly.
+
+// T: Copy: Ensures the type can be duplicated without calling clone().
+
+// Both pos1 and pos2 get the same values because Copy just duplicates the data.
+
