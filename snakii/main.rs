5f86b8f6d58a1e63098abceb7fd74b48fbd39132
Xharry01X <harshitsingh50621@gmail.com>
2025-03-03 13:48:58 +0530

    trait -1

--- snakii/main.rs
+++ snakii/main.rs
@@ -1,30 +1,17 @@
-trait Travel {
-    fn move_it(&self);
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct Point {
+    x:  i32,
+    y: i32,
 }
 
-enum Vehicle {
-    Car(String),
-    Bike(i32),
-    Boat
-}
-
-impl Travel for Vehicle {
-    fn move_it(&self) {
-        match self {
-            Vehicle::Car(name) => println!("My car name is {}",name),
-            Vehicle::Bike(num) => println!("Car speed is {}",num),
-            Vehicle::Boat => println!("Just use boat anyway")
-        }
-    }
+fn print_debug<T: Debug>(value: T){
+    println!("{:?}", value);
 }
 
 fn main(){
-    let car = Vehicle::Car(String::from("BMW"));
-    let bike = Vehicle::Bike(25);
-    let boat = Vehicle::Boat;
+    let p =Point { x: 4, y:5 };
 
-    car.move_it();
-    bike.move_it();
-    boat.move_it();
-
+    print_debug(p);
 }
